8816002835ac2c0efdebbadf9fc3cecef220018c
BlueAnanas <Taras.Sajuk@gmail.com>
2014-05-07 09:54:48 +0200

    Initial (empty) release 0.9

---
+++ src/main/java/eu/sajuk/tsdev/forgecmods/forgemod42/ForgeMod42.java
@@ -0,0 +1,11 @@
+package eu.sajuk.tsdev.forgecmods.forgemod42;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ForgeMod42.MODID, version = ForgeMod42.VERSION)
+public class ForgeMod42
+{
+    public static final String MODID = "forgemod42";
+    public static final String VERSION = "0.9";
+
+}
